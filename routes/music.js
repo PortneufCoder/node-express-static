63afb1a2972517e38f02be1c0ff0c1e4c7696405
Victor Ajani <32583833+PortneufCoder@users.noreply.github.com>
2018-12-18 10:33:23 -0500

    Update music.js
    Edited rootDrive => 'rootPath'

--- routes/music.js
+++ routes/music.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const rootPath = require('..utils/path');
 
 router.get("/music", (req, res, next) => {
-  res.sendFile(path.join(rootDrive, 'public', 'music.html')); // replaces native node responses
+  res.sendFile(path.join(rootPath, 'public', 'music.html')); // replaces native node responses
   
 }); // __dirname points to the folder holding the file where you construct it e.g routes
 
